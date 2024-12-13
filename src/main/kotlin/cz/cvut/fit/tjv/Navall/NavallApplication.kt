5e3efa3fb345039ad5ab8a2ed0909c38e5c01977
Matej Skalicky <matej.skalickyy@gmail.com>
2024-12-13 17:45:44 +0100

    gradle kotlin init

---
+++ src/main/kotlin/cz/cvut/fit/tjv/Navall/NavallApplication.kt
@@ -0,0 +1,11 @@
+package cz.cvut.fit.tjv.Navall
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NavallApplication
+
+fun main(args: Array<String>) {
+	runApplication<NavallApplication>(*args)
+}
---
+++ src/test/kotlin/cz/cvut/fit/tjv/Navall/NavallApplicationTests.kt
@@ -0,0 +1,13 @@
+package cz.cvut.fit.tjv.Navall
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NavallApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
