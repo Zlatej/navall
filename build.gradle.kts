33ce95bdad375f304e8c09bf30913ec9c0a8a53b
Matej Skalicky <matej.skalickyy@gmail.com>
2024-12-19 20:34:37 +0100

    added htmx and thymeleaf

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("io.github.wimdeblauwe:htmx-spring-boot-thymeleaf:4.0.1")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("org.postgresql:postgresql")
