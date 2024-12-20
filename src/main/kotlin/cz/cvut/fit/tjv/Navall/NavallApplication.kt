2a45f6b17f048d5cc1a4a966ac5fc0cdd0e89740
Matej Skalicky <matej.skalickyy@gmail.com>
2024-12-20 14:02:06 +0100

    adde logger

--- src/main/kotlin/cz/cvut/fit/tjv/Navall/NavallApplication.kt
+++ src/main/kotlin/cz/cvut/fit/tjv/Navall/NavallApplication.kt
@@ -1,11 +1,15 @@
 package cz.cvut.fit.tjv.Navall
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+
+private var log = KotlinLogging.logger {}
 
 @SpringBootApplication
 class NavallApplication
 
 fun main(args: Array<String>) {
-	runApplication<NavallApplication>(*args)
+    runApplication<NavallApplication>(*args)
+    log.info { "App is running, now NavalLove! <3" }
 }
