20545384da24ff4a57356de513160b5499ce2410
Matej Skalicky <matej.skalickyy@gmail.com>
2025-01-21 23:14:20 +0100

    logging

--- src/main/kotlin/cz/cvut/fit/tjv/Navall/error/GlobalExceptionHandler.kt
+++ src/main/kotlin/cz/cvut/fit/tjv/Navall/error/GlobalExceptionHandler.kt
@@ -1,13 +1,14 @@
 package cz.cvut.fit.tjv.Navall.error
 
 import io.github.oshai.kotlinlogging.KotlinLogging
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.server.ResponseStatusException
 import java.time.LocalDateTime
 
-private val logger = KotlinLogging.logger {}
+private val log = KotlinLogging.logger {}
 
 @ControllerAdvice
 class GlobalExceptionHandler {
@@ -20,10 +21,12 @@ class GlobalExceptionHandler {
             error = e.statusCode.toString(),
         )
 
-        if (e.statusCode.value() >= 500) {
-            logger.error(e) {}
+        if (e.statusCode == HttpStatus.INTERNAL_SERVER_ERROR) {
+            log.error(e) {}
         }
-
+        else {
+            log.warn(e) {}
+        }
         return ResponseEntity(errorResponse, e.statusCode)
     }
 }
