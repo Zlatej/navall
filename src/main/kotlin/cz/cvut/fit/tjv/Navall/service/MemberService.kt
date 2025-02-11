4278cee7b6f15da81cf6df0c6069da377144c749
Matej Skalicky <matej.skalickyy@gmail.com>
2025-02-11 19:57:08 +0100

    some more refactoring and reformatting2

--- src/main/kotlin/cz/cvut/fit/tjv/Navall/service/MemberService.kt
+++ src/main/kotlin/cz/cvut/fit/tjv/Navall/service/MemberService.kt
@@ -1,9 +1,9 @@
 package cz.cvut.fit.tjv.Navall.service
 
 import cz.cvut.fit.tjv.Navall.models.Member
+import cz.cvut.fit.tjv.Navall.models.dtos.MemberDto
 import cz.cvut.fit.tjv.Navall.repository.GroupRepo
 import cz.cvut.fit.tjv.Navall.repository.MemberRepo
-import cz.cvut.fit.tjv.Navall.service.dtos.MemberDto
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.web.server.ResponseStatusException
@@ -13,28 +13,34 @@ class MemberService(
     private val memberRepo: MemberRepo,
     private val groupRepo: GroupRepo,
 ) {
-    fun getMembers(): List<Member> = memberRepo.findAll()
+    fun getAllMembers(): List<Member> = memberRepo.findAll()
 
     fun getMembersOfGroup(id: Long): List<Member> = memberRepo.findAllByGroup_Id(id)
 
     fun getMember(id: Long): Member = memberRepo.getMemberById(id) ?: throw ResponseStatusException(
-        HttpStatus.NOT_FOUND,
-        "Member with ID $id not found"
+        HttpStatus.NOT_FOUND, "Member with ID $id not found"
     )
 
+    fun getMembers(ids: List<Long>): List<Member> {
+        val members = memberRepo.findAllById(ids)
+        if (members.size != ids.size) {
+            throw ResponseStatusException(
+                HttpStatus.NOT_FOUND, "One or more member IDs do not exist"
+            )
+        }
+        return members
+    }
+
     fun getMemberByEmail(email: String) = memberRepo.getMemberByEmail(email) ?: throw ResponseStatusException(
-        HttpStatus.NOT_FOUND,
-        "Email $email not found"
+        HttpStatus.NOT_FOUND, "Email $email not found"
     )
 
     fun createMember(memberDto: MemberDto): Member {
         if (memberRepo.existsMemberByEmail(memberDto.email)) throw ResponseStatusException(
-            HttpStatus.BAD_REQUEST,
-            "Email already exists"
+            HttpStatus.BAD_REQUEST, "Email already exists"
         )
         val group = groupRepo.findGroupById(memberDto.groupId) ?: throw ResponseStatusException(
-            HttpStatus.NOT_FOUND,
-            "Group ${memberDto.groupId} not found"
+            HttpStatus.NOT_FOUND, "Group ${memberDto.groupId} not found"
         )
         val member = Member(
             name = memberDto.name,
@@ -47,8 +53,7 @@ class MemberService(
 
     fun updateMember(memberDto: MemberDto): Member {
         val existingMember = memberDto.id?.let { memberRepo.getMemberById(it) } ?: throw ResponseStatusException(
-            HttpStatus.NOT_FOUND,
-            "Member ${memberDto.id} not found"
+            HttpStatus.NOT_FOUND, "Member ${memberDto.id} not found"
         )
         val updatedMember = existingMember.copy(
             name = memberDto.name,
@@ -58,11 +63,11 @@ class MemberService(
         return updatedMember
     }
 
-    fun increaseBalance(member: Member, change: Double) {
+    fun increaseBalance(member: Member, change: Double): Member {
         val updatedMember = member.copy(
             balance = member.balance + change,
         )
-        memberRepo.save(updatedMember)
+        return memberRepo.save(updatedMember)
     }
 
     fun decreaseBalance(member: Member, change: Double) {
